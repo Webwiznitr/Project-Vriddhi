0fd2f1bc9e6e864ec20d383f71b51760d90fe048
a-sambhab <saisambhav21chaini@gmail.com>
2022-10-12 00:00:52 +0530

    Register added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import LandingPage from "./pages/LandingPage";
 import "./App.css";
 import {Routes, Route} from 'react-router-dom';
 import Tournament from "./pages/Tournament";
+import Register from './pages/Register';
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<LandingPage/>} />
       <Route path="/tournament" element={<Tournament/>} />
+      <Route path="/register" element={<Register/>}/>
     </Routes>
     </>
   );
