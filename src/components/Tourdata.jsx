8b91e86cf112d4d198bf3d3eba2fdeddd412f699
a-sambhab <saisambhav21chaini@gmail.com>
2022-10-12 02:16:23 +0530

    Significant Changes

--- src/components/Tourdata.jsx
+++ src/components/Tourdata.jsx
@@ -1,47 +1,47 @@
 const Tourdata = [
     {
         name: "Football",
-        img: "",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520805/xFootball.png.pagespeed.ic.S4QDdILv-5_auv6wx.png",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Cricket",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520802/xCricket.png.pagespeed.ic.vbSmqDZ22W_nrgwvg.png",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Basketball",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520800/xBasketball.png.pagespeed.ic._RrnaK_aRY_c4qrvf.png",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Badminton",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520782/kabaddi_artwork_jt29t1.svg",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Kabaddi",
+        img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1664966819/IMG_20221003_211203_621_beutqw.png",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Kho-Kho",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520774/image-removebg-preview_2_indoen.png",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Table Tennis",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520772/4307571_iaz1mc.jpg",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Volleyball",
+        img: "https://res.cloudinary.com/dgy8ybeoy/image/upload/v1665520769/1000_F_66774759_rTBWvnE8yhLBSppvZWJxebK6LzqC4VKl_nvvjdh.jpg",
         info: ""
     },
     {
-        name: "Football",
-        img: "",
+        name: "Chess",
+        img: "https://res.cloudinary.com/dcbco6i8s/image/upload/v1664966819/IMG_20221003_211203_621_beutqw.png",
         info: ""
     },
 ]
--- src/pages/Tournament.js
+++ src/pages/Tournament.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
+import Tourcard from '../components/Tourcard';
+import Tourdata from '../components/Tourdata';
 function Tournament() {
+    const [selected, setselected] = useState(false)
+    const [data, setdata] = useState({})
+    const Selected = (props) => {
+        setselected(true);
+        setdata(props);
+        console.log(data);
+    }
   return (
     <>
-    <div  className="bg-[#111a21] h-full min-h-screen text-white flex flex-col content-evenly">
+    <div  className="bg-[#111a21] h-fit min-h-screen text-white flex flex-col content-evenly">
         <div className='flex flex-col w-full h-screen'>
         <Navbar/>
-            <div className='w-full h-1/6 flex justify-center items-center'>
+            <div className='w-full h-[10%] flex justify-center items-center'>
                 <div className='w-1/2 h-[90%] border border-white text-[70px] tracking-widest text-center mt-0 flex justify-center items-center'> TOURNAMENT </div>
             </div>
-            <div className='w-full h-5/6 flex flex-row'>
-                <div className='w-2/3 h-full flex justify-center items-center' >
-                    <div className='w-[80%] h-[97%] border border-white' >
-                        
+            <div className='w-full h-[90%] flex flex-row'>
+                <div className='w-[60%] h-full flex justify-center items-center' >
+                    <div className='w-[80%] h-[97%] border border-white grid grid-cols-3 gap-[5px] p-1' >
+                        {Tourdata.map((tour)=>{
+                            return(
+                                <Tourcard
+                                    name={tour.name}
+                                    img={tour.img}
+                                    info={tour.info}
+                                    select={Selected}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
-                <div className='w-1/3 h-full flex justify-center items-center' >
-                    <div className='w-[90%] h-[90%] border border-white' ></div>
+                <div className='w-[40%] h-full flex justify-center items-center' >
+                    <div className='w-[90%] h-[90%] border border-white flex justify-center items-center' >
+                    {selected ? <>
+                        <div className='w-[98%] h-[98%] border-[8px] border-white'>
+                        {/* <div className='flex'> */}
+                            <img src={data.img} alt="sport" className='relative -top-5 -left-5 w-[50%]'></img>
+                            <div className='w-[50%] h-1/2 relative -top-5 -right-0'>
+                                DATE: TBA
+                            </div>
+                        {/* </div> */}
+                        </div>
+                    </> : 
+                        <img src="https://res.cloudinary.com/dcbco6i8s/image/upload/v1664966819/IMG_20221003_211203_621_beutqw.png" alt="background" className='w-[90%]'></img>}
+                    </div>
                 </div>
             </div>
         </div>
