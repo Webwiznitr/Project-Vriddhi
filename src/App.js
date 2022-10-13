bcbe7770ea89870ed15648206fcc75184f44f92c
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-10-14 00:46:40 +0530

    Gallery Page added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage";
 import "./App.css";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Tournament from "./pages/Tournament";
-import Register from './pages/Register';
+import Register from "./pages/Register";
+import Gallery from "./pages/Gallery";
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<LandingPage/>} />
-      <Route path="/tournament" element={<Tournament/>} />
-      <Route path="/register" element={<Register/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/tournament" element={<Tournament />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Gallerydata.jsx
@@ -0,0 +1,40 @@
+const Gallerydata = [
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682946/PJ10_2_ywrzub.jpg",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682946/PJ7_1_eoqfme.jpg",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682946/PJ7_2_jniu48.jpg",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/images_5_8__060644_d91fjz.png",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/62c55f8792f924859563cb8d_060037_mmleik.png",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/62c55fc792f924859563cc8d_1_060102_aprwpz.png",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/img_1_1663626525057_060652_qakagz.png",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/images_5_7__060634_fff6bi.png",
+    alt: "",
+  },
+  {
+    img: "https://res.cloudinary.com/ash123/image/upload/v1665682945/images_5_6__060628_ozs4lr.png",
+    alt: "",
+  },
+];
+
+export default Gallerydata;
