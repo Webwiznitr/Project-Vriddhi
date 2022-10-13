23ed343f129c4c5f1f9045894640546633cfff71
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-10-14 00:55:04 +0530

    Gallery changes made

--- src/components/Galleryphotos.jsx
+++ src/components/Galleryphotos.jsx
@@ -7,7 +7,7 @@ const Galleryphotos = () => {
       <h1 className="text-5xl font-bold text-white py-4 text-center">PHOTOS</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center md:gap-8 lg:gap-0">
         {Gallerydata.map((data) => (
-          <div className="my-4 w-80 lg:w-80 xl:w-96 ">
+          <div className="my-4 w-80 lg:w-80 xl:w-96 " key={data.key}>
             <img
               src={data.img}
               alt={data.alt}
