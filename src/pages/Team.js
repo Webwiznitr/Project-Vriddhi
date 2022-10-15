ffd2c3d3925cc62bf0982dac989f9cc1fcbaa6d7
Sai Sambhab <84642011+a-sambhab@users.noreply.github.com>
2022-10-15 19:33:30 +0530

    Update Team.js

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -5,7 +5,8 @@ import '../components/TeamCard.css'
 
 
 const Team = () => {
-  const [data, setData] = useState(TeamData);
+  const [data, setData] = useState({});
+  setData(TeamData);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-4 md:p-8 pt-4 bg-[#02022b]">
       {data.map((person) => {
