9ab670f409af1d4132a729444d6a2c625636bfc8
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-10-16 19:06:59 +0530

    Team page updated

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import TeamData from "../components/TeamData";
 import TeamCard from "../components/TeamCard";
-import '../components/TeamCard.css'
-
+import "../components/TeamCard.css";
 
 const Team = () => {
-  const [data, setData] = useState({});
-  setData(TeamData);
+  const [data, setData] = useState(TeamData);
+  // setData(TeamData);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-4 md:p-8 pt-4 bg-[#02022b]">
       {data.map((person) => {
